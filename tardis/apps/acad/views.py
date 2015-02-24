833d91a8d0199b00943e541b7b2b0c016e30c596
Jianfeng Li <li@redboxresearchdata.com.au>
2015-02-24 16:58:32 +1030

    Add dataset url to acad app
    for easy testing before integrate into tardis.
    url: apps/acad/dataset/7/

--- tardis/apps/acad/views.py
+++ tardis/apps/acad/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Source
+from tardis.tardis_portal.models import Dataset
+from .models import Source, Analysis, Processing
+
+def dataset(request, id):
+    dataset = get_object_or_404(Dataset, pk=id)
+    context = {'dataset': dataset, 'analysis': dataset.analysis}
+    return render(request, 'dataset.html', context)
 
 def source_index(request):
     context = {'source_list': Source.objects.all()}
@@ -9,4 +15,4 @@ def source_detail(request, id):
     source = get_object_or_404(Source, pk=id)
     samples = source.sample_set.all()
     context = {'source': source, 'samples': samples}
-    return render(request, 'source/detail.html', context) 
+    return render(request, 'source/detail.html', context)
