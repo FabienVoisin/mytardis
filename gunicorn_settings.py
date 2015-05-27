b1ef0640b0b097a7193d931b6f7a418695201ea0
Craig Patten <cjp@cjp.id.au>
2015-05-27 13:46:57 +0930

    Increase the process count for larger VMs.

--- gunicorn_settings.py
+++ gunicorn_settings.py
@@ -1,6 +1,7 @@
+import multiprocessing
 import os
 
-workers = 16
+workers = max(16, multiprocessing.cpu_count() * 8)
 worker_class = "sync"
 timeout = 24 * 60 * 60
 chdir = os.path.dirname(__file__)
